27cf366907a5f2ed26254ef2904f01e919eee7d2
FKouhai <frandres00@gmail.com>
2025-05-15 13:07:30 +0200

    add: clap features

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,64 @@
 use anyhow::Result;
+use clap::Parser;
+use core::fmt;
 use futures_lite::StreamExt;
+use iroh::{Endpoint, NodeAddr, NodeId};
 use iroh::protocol::Router;
-use iroh::{Endpoint, NodeId};
 use iroh_gossip::net::{Event,Gossip,GossipEvent,GossipReceiver};
 use iroh_gossip::proto::TopicId;
 use serde::{Deserialize,Serialize};
 use std::collections::HashMap;
+use std::str::FromStr;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[clap(short, long)]
+    name: Option<String>,
+
+    #[clap(short,long,default_value = "0")]
+    bind_port: u16,
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Parser, Debug)]
+enum Command {
+    Open,
+    Join {
+        ticket: String,
+    },
+}
+
+#[derive(Debug,Serialize,Deserialize)]
+struct Ticket {
+    topic: TopicId,
+    nodes: Vec<NodeAddr>,
+}
+
+impl Ticket {
+    fn from_bytes(bytes: &[u8]) -> Result<Self> {
+        serde_json::from_slice(bytes).map_err(Into::into)
+    }
+    pub fn to_bytes(&self) -> Vec<u8>{
+        serde_json::to_vec(self).expect("serde_json::to_vec is infallible")
+    }
+}
+
+impl fmt::Display for Ticket {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut text = data_encoding::BASE32_NOPAD.encode(&self.to_bytes()[..]);
+        text.make_ascii_lowercase();
+        write!(f, "{}", text)
+    }
+}
+
+impl FromStr for Ticket {
+    type Err = anyhow::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let bytes = data_encoding::BASE32_NOPAD.decode(s.to_ascii_uppercase().as_bytes())?;
+        Self::from_bytes(&bytes)
+    }
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Message {
@@ -70,42 +123,89 @@ fn input_loop(line_tx: tokio::sync::mpsc::Sender<String>) -> Result<()> {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let endpoint = Endpoint::builder()
-        .discovery_n0()
-        .bind()
-        .await?;
-    print!("> node id: {}", endpoint.node_id());
+    let args = Args::parse();
 
+    // parse the cli command
+    let (topic, nodes) = match &args.command {
+        Command::Open => {
+            let topic = TopicId::from_bytes(rand::random());
+            println!("> opening chat room for topic {topic}");
+            (topic, vec![])
+        }
+        Command::Join { ticket } => {
+            let Ticket { topic, nodes } = Ticket::from_str(ticket)?;
+            println!("> joining chat room for topic {topic}");
+            (topic, nodes)
+        }
+    };
+
+    let endpoint = Endpoint::builder().discovery_n0().bind().await?;
+
+    println!("> our node id: {}", endpoint.node_id());
     let gossip = Gossip::builder().spawn(endpoint.clone()).await?;
 
     let router = Router::builder(endpoint.clone())
         .accept(iroh_gossip::ALPN, gossip.clone())
         .spawn();
 
-    let id = TopicId::from_bytes(rand::random());
-    let node_ids = vec![];
-    let topic = gossip.subscribe(id, node_ids)?;
-    // Splitting the topic into a Gossip sender and a Gossip receiver
-    let (sender, receiver) = topic.split();
-    let message = Message::new(MessageBody::AboutMe {
-        from: endpoint.node_id(),
-        name: String::from("franky"),
-    });
-    sender.broadcast(message.to_vec().into()).await?;
-    tokio::spawn(subscribe_loop(receiver));
-    let (line_tx, mut line_rx) = tokio::sync::mpsc::channel(1);
-    std::thread::spawn(move || input_loop(line_tx));
+    // in our main file, after we create a topic `id`:
+    // print a ticket that includes our own node id and endpoint addresses
+    let ticket = {
+        // Get our address information, includes our
+        // `NodeId`, our `RelayUrl`, and any direct
+        // addresses.
+        let me = endpoint.node_addr().await?;
+        let nodes = vec![me];
+        Ticket { topic, nodes }
+    };
+    println!("> ticket to join us: {ticket}");
 
-    println!("> type a message and hit enter to broadcast...");
-    while let Some(text) = line_rx.recv().await {
-        let message = Message::new(MessageBody::Message {
+    // join the gossip topic by connecting to known nodes, if any
+    let node_ids = nodes.iter().map(|p| p.node_id).collect();
+    if nodes.is_empty() {
+        println!("> waiting for nodes to join us...");
+    } else {
+        println!("> trying to connect to {} nodes...", nodes.len());
+        // add the peer addrs from the ticket to our endpoint's addressbook so that they can be dialed
+        for node in nodes.into_iter() {
+            endpoint.add_node_addr(node)?;
+        }
+    };
+    let (sender, receiver) = gossip.subscribe_and_join(topic, node_ids).await?.split();
+    println!("> connected!");
+
+    // broadcast our name, if set
+    if let Some(name) = args.name {
+        let message = Message::new(MessageBody::AboutMe {
             from: endpoint.node_id(),
-            text: text.clone(), 
+            name,
         });
         sender.broadcast(message.to_vec().into()).await?;
-        println!("> me: {text}");
     }
 
+    // subscribe and print loop
+    tokio::spawn(subscribe_loop(receiver));
+
+    // spawn an input thread that reads stdin
+    // create a multi-provider, single-consumer channel
+    let (line_tx, mut line_rx) = tokio::sync::mpsc::channel(1);
+    // and pass the `sender` portion to the `input_loop`
+    std::thread::spawn(move || input_loop(line_tx));
+
+    // broadcast each line we type
+    println!("> type a message and hit enter to broadcast...");
+    // listen for lines that we have typed to be sent from `stdin`
+    while let Some(text) = line_rx.recv().await {
+        // create a message from the text
+        let message = Message::new(MessageBody::Message {
+            from: endpoint.node_id(),
+            text: text.clone(),
+        });
+        // broadcast the encoded message
+        sender.broadcast(message.to_vec().into()).await?;
+        // print to ourselves the text that we sent
+        println!("> sent: {text}");
+    }
     router.shutdown().await?;
 
     Ok(())
