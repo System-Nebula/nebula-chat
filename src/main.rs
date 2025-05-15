fe55058083a25f8a0f6fe4deba2dd9db575fc658
FKouhai <frandres00@gmail.com>
2025-05-15 09:10:41 +0200

    fix(cargo.toml) pin rand version
    the specified rand version is needed in order to generate a node id.
    more up to date versions do not implement the needed trait for iroh to
    function properly

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use iroh::{Endpoint,SecretKey};
+
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let secret_key = SecretKey::generate(rand::rngs::OsRng);
+    let endpoint = Endpoint::builder().secret_key(secret_key)
+        .discovery_n0()
+        .bind()
+        .await?;
+    print!("> node id: {}", endpoint.node_id());
+    Ok(())
+
 }
